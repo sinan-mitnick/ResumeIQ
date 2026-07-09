5831eebe4f3e9d74d70591d9354b497fdb38b098
sinan <sinan14102022.com>
2026-07-09 06:13:59 +0530

    Fix production CORS

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,19 +1,9 @@
 import axios from "axios";
 
+console.log("API URL =", import.meta.env.VITE_API_URL);
+
 const api = axios.create({
-  baseURL:
-    import.meta.env.VITE_API_URL ||
-    "http://localhost:5000/api",
-});
-
-api.interceptors.request.use((config) => {
-  const token = localStorage.getItem("token");
-
-  if (token) {
-    config.headers.Authorization = `Bearer ${token}`;
-  }
-
-  return config;
+  baseURL: import.meta.env.VITE_API_URL || "http://localhost:5000/api",
 });
 
 export default api;
