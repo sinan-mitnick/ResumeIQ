6401bb1b6176d3aa06dd455d1a5bc8c07de4d2c7
sinan <sinan14102022.com>
2026-07-12 16:50:12 +0530

    Configure Nodemailer with Brevo SMTP

--- server/utils/sendEmail.js
+++ server/utils/sendEmail.js
@@ -2,7 +2,7 @@ const nodemailer = require("nodemailer");
 
 const transporter = nodemailer.createTransport({
     host: "smtp-relay.brevo.com",
-    port: 587,
+    port: 465,
     secure: false,
     auth: {
         user: process.env.EMAIL_USER,
